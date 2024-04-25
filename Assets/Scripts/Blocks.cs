018c2194f5620627b018ba4a39e756133d5bb018
SalihBahadirGulmez <115195096+SalihBahadirGulmez@users.noreply.github.com>
2024-04-25 04:01:25 +0300

    25.04.2024

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    public List<GameObject> blueBlocks = new List<GameObject>();
+    public List<GameObject> greenBlocks = new List<GameObject>();
+    public List<GameObject> pinkBlocks = new List<GameObject>();
+    public List<GameObject> purpleBlocks = new List<GameObject>();
+    public List<GameObject> redBlocks = new List<GameObject>();
+    public List<GameObject> yellowBlocks = new List<GameObject>();
+    public List<List<GameObject>> blocks2d = new List<List<GameObject>>();
+
+    public int row = 9;
+    public int column = 9;
+    public int numberOfColors = 6;
+    public int conditionA = 4;
+    public int conditionB = 6;
+    public int conditionC = 7;
+
+    private int blockNum;
+    public int height = 14;
+
+    public List<List<int>> indexArray;
+    public List<List<GameObject>> blockArray = new List<List<GameObject>>();
+    public List<List<int>> blockArrayAsNum = new List<List<int>>();
+
+    int tempRandomNumber;
+    GameObject block;
+
+    List<List<int>> _indexArray = new List<List<int>>();
+    List<int> subIndexArray;
+
+    int posX;
+    int posY;
+
+    Vector2 tempPos;
+
+    private void Start()
+    {
+        blocks2d.Add(blueBlocks);
+        blocks2d.Add(greenBlocks);
+        blocks2d.Add(pinkBlocks);
+        blocks2d.Add(purpleBlocks);
+        blocks2d.Add(redBlocks);
+        blocks2d.Add(yellowBlocks);
+
+        Camera.main.transform.position = new Vector3((column / 2f) - 0.5f, 0, -10);
+               
+        CreateBlocks();      
+        indexArray = GroupFinder();
+        Shuffle(indexArray);
+        Conditions();
+    }
+
+    public void CreateBlocks()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            blockArrayAsNum.Add(new List<int>());
+            blockArray.Add(new List<GameObject>());
+            for (int j = 0; j < column; j++)
+            {
+                tempRandomNumber = Random.Range(0, numberOfColors);
+                blockArrayAsNum[i].Add(tempRandomNumber);
+                block = Instantiate(blocks2d[tempRandomNumber][0], new Vector2(j, i + height), Quaternion.identity);
+                block.name = i + "" + j;
+                block.GetComponent<SpriteRenderer>().sortingOrder = i;
+                blockArray[i].Add(block);                             
+            }
+        }
+    }
+
+    public List<List<int>> GroupFinder()//1.eleman blok türü olacak şekilde indexarray'e grupların indexlerini array şeklinde ekleyip en son tüm bu gruparı döndürüyor.
+    {
+        _indexArray.Clear();
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                subIndexArray = new List<int>();
+
+                if (IsUnique(_indexArray, i, j))
+                {
+                    blockNum = blockArrayAsNum[i][j];
+                    subIndexArray.Add(blockNum);
+                    subIndexArray.Add(i);
+                    subIndexArray.Add(j);
+
+                    SearchNeighbors(subIndexArray, i, j);
+
+                    _indexArray.Add(subIndexArray);
+                }                        
+            }
+        }
+        return _indexArray;
+    }
+
+    public void SearchNeighbors(List<int> _subIndexArray, int _row, int _column)//komşulara bakıp grup varsa subindex e ekliyor
+    {
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (i != -j && i != j 
+                    && _row + i >= 0 && _row + i < row 
+                    && _column + j >= 0 && _column + j < column 
+                    && blockArrayAsNum[_row + i][_column + j] == blockNum)
+                {
+                    if (IsUniqueV2(_subIndexArray, _row, _column, i, j))
+                    {
+                        _subIndexArray.Add(_row + i);
+                        _subIndexArray.Add(_column + j);
+                        SearchNeighbors(_subIndexArray, _row + i, _column + j);
+                    }
+                }
+            }
+        }        
+    }
+
+    public bool IsUniqueV2(List<int> _subIndexArray, int _row, int _column, int neighbourR, int neighbourC) //searchneighbors da kullanılıyor, subindexarray'e eklenmesi planlanan indexler daha önceden eklendi mi diye kontrol ediyor. olmadığı durumda recursion dunc nedeniyle sonsuz döngüye girebilir.
+    {          
+        for (int i = 1; i < _subIndexArray.Count; i += 2)
+        {
+            if (_subIndexArray[i] == _row + neighbourR && _subIndexArray[i + 1] == _column + neighbourC)
+            {
+                return false;                    
+            }
+        }               
+        return true;
+    }
+
+    public bool IsUnique(List<List<int>> _indexArray, int _row, int _column)//groupfinder'da kullanılıyor, indexarrayde aynı grubun 1den fazla olmasını engelliyor.
+    {
+        for (int i = 0; i < _indexArray.Count; i++)
+        {
+            for (int j = 1; j < _indexArray[i].Count; j += 2)
+            {
+                if (_indexArray[i][j] == _row && _indexArray[i][j + 1] == _column)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+        
+    public void Conditions()
+    {
+        for (int i = 0; i < indexArray.Count; i++)
+        {
+            if (indexArray[i].Count > conditionC * 2)
+            {
+                for (int j = 1; j < indexArray[i].Count; j += 2)
+                {
+                    posY = indexArray[i][j];
+                    posX = indexArray[i][j + 1];
+                    tempPos = blockArray[posY][posX].transform.position;
+                    Destroy(blockArray[posY][posX]);
+                    block = Instantiate(blocks2d[indexArray[i][0]][3], tempPos, Quaternion.identity);
+                    block.name = posY + "" + posX;
+                    block.GetComponent<SpriteRenderer>().sortingOrder = posY;
+                    blockArray[posY][posX] = block;
+                }
+            }
+            else if (indexArray[i].Count > conditionB * 2)
+            {
+                for (int j = 1; j < indexArray[i].Count; j += 2)
+                {
+                    posY = indexArray[i][j];
+                    posX = indexArray[i][j + 1];
+                    tempPos = blockArray[posY][posX].transform.position;                    
+                    Destroy(blockArray[posY][posX]);
+                    block = Instantiate(blocks2d[indexArray[i][0]][2], tempPos, Quaternion.identity);
+                    block.name = posY + "" + posX;
+                    block.GetComponent<SpriteRenderer>().sortingOrder = posY;
+                    blockArray[posY][posX] = block;
+                }
+            }
+            else if (indexArray[i].Count > conditionA * 2)
+            {
+                for (int j = 1; j < indexArray[i].Count; j += 2)
+                {
+                    posY = indexArray[i][j];
+                    posX = indexArray[i][j + 1];
+                    tempPos = blockArray[posY][posX].transform.position;
+                    Destroy(blockArray[posY][posX]);
+                    block = Instantiate(blocks2d[indexArray[i][0]][1], tempPos, Quaternion.identity);
+                    block.name = posY + "" + posX;
+                    block.GetComponent<SpriteRenderer>().sortingOrder = posY;
+                    blockArray[posY][posX] = block;
+                }
+            }
+            else
+            {
+                for (int j = 1; j < indexArray[i].Count; j += 2)
+                {
+                    posY = indexArray[i][j];
+                    posX = indexArray[i][j + 1];
+                    tempPos = blockArray[posY][posX].transform.position;
+                    Destroy(blockArray[posY][posX]);
+                    block = Instantiate(blocks2d[indexArray[i][0]][0], tempPos, Quaternion.identity);
+                    block.name = posY + "" + posX;
+                    block.GetComponent<SpriteRenderer>().sortingOrder = posY;
+                    blockArray[posY][posX] = block;
+                }
+            }
+
+        }
+    }
+    
+    public void Shuffle(List<List<int>> _indexArray)//hiç grup olmaması durumunda blokları silip rastgele tekrar oluşturuyor.
+    {  
+        for (int i = 0; i < _indexArray.Count; i++)
+        {
+            if (indexArray[i].Count > 3)
+            {
+                return;
+            }
+        }
+        RemoveBlocks(blockArray);
+        CreateBlocksV2();
+        indexArray = GroupFinder();
+        Shuffle(indexArray);
+    }
+
+    public void RemoveBlocks(List<List<GameObject>> _blockArray)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                Destroy(_blockArray[i][j]);
+            }
+        }
+    }
+
+    public void CreateBlocksV2()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                int tempRandomNumber = Random.Range(0, numberOfColors);
+                blockArrayAsNum[i][j] = tempRandomNumber;
+                block = Instantiate(blocks2d[tempRandomNumber][0], new Vector2(j, i + height), Quaternion.identity);
+                block.name = i + "" + j;
+                block.GetComponent<SpriteRenderer>().sortingOrder = i;
+                blockArray[i][j] = block;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BlocksCollapse.cs
@@ -0,0 +1,226 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksCollapse : MonoBehaviour
+{
+    Blocks blocksSc;
+    private bool reBuild;
+    GameObject block;
+    int tempNum1;
+    int tempNum2;
+    Vector2 tempPosition;
+    void Start()
+    {
+        blocksSc = GameObject.Find("Blocks").GetComponent<Blocks>();
+    }
+
+    void OnMouseDown()
+    {
+        DestroyObject();
+        if (reBuild)
+        {
+            blocksSc.indexArray = blocksSc.GroupFinder();
+            blocksSc.Shuffle(blocksSc.indexArray);
+            blocksSc.Conditions();
+        }
+    }
+
+    public void DestroyObject()
+    {
+        reBuild = false;
+        for (int i = 0; i < blocksSc.indexArray.Count; i++)
+        {
+            if (blocksSc.indexArray[i].Count > 3)
+            {
+                for (int j = 1; j < blocksSc.indexArray[i].Count; j += 2)
+                {
+                    if (blocksSc.indexArray[i][j] + "" + blocksSc.indexArray[i][j + 1] == gameObject.name)
+                    {
+                        for (int k = 1; k < blocksSc.indexArray[i].Count; k += 2)
+                        {                          
+                            tempNum1 = blocksSc.indexArray[i][k];
+                            tempNum2 = blocksSc.indexArray[i][k + 1];
+                            Destroy(blocksSc.blockArray[tempNum1][tempNum2]);
+                            blocksSc.blockArrayAsNum[tempNum1][tempNum2] = 10;
+                        }
+                        reBuild = true;
+                        ReBuild();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void DestroyObject2() //todo düzenle
+    {
+        reBuild = false;
+        for (int i = 0; i < blocksSc.indexArray.Count; i++)
+        {
+            if (blocksSc.indexArray[i].Count > 3)
+            {
+                for (int j = 1; j < blocksSc.indexArray[i].Count; j += 2)
+                {
+                    if (blocksSc.indexArray[i][j] + "" + blocksSc.indexArray[i][j + 1] == gameObject.name)
+                    {
+                        for (int k = 1; k < blocksSc.indexArray[i].Count; k += 2)
+                        {
+                            List<int> temp = new List<int>();
+                            temp.Add(blocksSc.indexArray[i][k]);
+                            temp.Add(blocksSc.indexArray[i][k + 1]);
+                            array.Add(temp);
+                            Destroy(blocksSc.blockArray[blocksSc.indexArray[i][k]][blocksSc.indexArray[i][k + 1]]);
+                        }
+                        reBuild = true;
+                        ReBuild2();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    List<List<int>> array = new List<List<int>>();
+    List<int> subArray = new List<int>();
+    int column;
+    int upperLimit;
+
+    public void ArrangeArrays(List<int> sortedSubArray, int currentIndex, int currentRow, int distance = 1)//todo düzenle
+    {
+        if (currentRow + distance < blocksSc.row - 1)
+        {
+            blocksSc.blockArrayAsNum[currentRow][column] = blocksSc.blockArrayAsNum[currentRow + distance][column];
+
+            blocksSc.blockArray[currentRow][column] = blocksSc.blockArray[currentRow + distance][column];
+            blocksSc.blockArray[currentRow][column].GetComponent<SpriteRenderer>().sortingOrder = currentRow;
+            blocksSc.blockArray[currentRow][column].name = currentRow + "" + column;
+        }
+        else return;
+        
+        if (currentIndex == sortedSubArray.Count - 1)
+        {
+            ArrangeArrays(sortedSubArray, currentIndex, currentRow +1, distance);
+        }
+        else if (currentRow == sortedSubArray[currentIndex + 1] - 1)
+        {
+            ArrangeArrays(sortedSubArray, currentIndex + 1, currentRow + 1, distance + 1);
+        }
+        else ArrangeArrays(sortedSubArray, currentIndex, currentRow + 1, distance);
+    }
+    public void ReBuild2() //todo düzenle
+    {
+        for (int i = 0; i < array.Count; i++)
+        {
+            if (array[i] == null) continue;
+            for (int j = i+1; j < array.Count; j++)
+            {
+                if (array[i][1] == array[j][1])
+                {
+                    subArray.Add(array[j][0]);
+                    array[j] = null;
+                }
+
+            }
+            column = array[i][1];
+            if (subArray.Count > 0)
+            {
+                subArray.Add(array[i][0]);
+                subArray.Sort();
+                for (int j = 0; j < subArray.Count; j++)
+                {
+                    if(j == subArray.Count - 1) upperLimit = blocksSc.row -1;
+                    else upperLimit = subArray[j + 1];
+
+                    for (int row = subArray[j]; row < upperLimit; row++)
+                    {
+
+                        blocksSc.blockArrayAsNum[row - j][column] = blocksSc.blockArrayAsNum[row + 1][column];
+
+                        blocksSc.blockArray[row - j][column] = blocksSc.blockArray[row + 1][column];
+                        blocksSc.blockArray[row - j][column].GetComponent<SpriteRenderer>().sortingOrder = row - j;
+                        blocksSc.blockArray[row - j][column].name = (row - j) + "" + column;
+                    }
+                }
+                for (int k = blocksSc.row - subArray.Count; k < blocksSc.row; k++)
+                {
+                    int tempRandomNumber = Random.Range(0, blocksSc.numberOfColors);
+                    blocksSc.blockArrayAsNum[k][column] = tempRandomNumber;
+                    Vector2 tempPosition = new Vector2(column, 2 + blocksSc.height);
+                    if (k - 1 >= 0 && blocksSc.blockArray[k - 1][column].transform.position.y > blocksSc.height)
+                    {
+                        tempPosition.y = blocksSc.blockArray[k - 1][column].transform.position.y + 2;
+                    }
+                    GameObject block = Instantiate(blocksSc.blocks2d[tempRandomNumber][0], tempPosition, Quaternion.identity);
+                    block.name = k + "" + column;
+                    block.GetComponent<SpriteRenderer>().sortingOrder = k;
+                    blocksSc.blockArray[k][column] = block;
+                }
+                subArray.Clear();
+            }
+            else
+            {
+                for (int row = array[i][0]; row < blocksSc.row - 1; row++)
+                {
+                    blocksSc.blockArrayAsNum[row][column] = blocksSc.blockArrayAsNum[row + 1][column];
+
+                    blocksSc.blockArray[row][column] = blocksSc.blockArray[row + 1][column];
+                    blocksSc.blockArray[row][column].GetComponent<SpriteRenderer>().sortingOrder = row;
+                    blocksSc.blockArray[row][column].name = row + "" + column;
+                }
+                int tempRandomNumber = Random.Range(0, blocksSc.numberOfColors);
+                blocksSc.blockArrayAsNum[blocksSc.row - 1][column] = tempRandomNumber;
+                Vector2 tempPosition = new Vector2(column, 2 + blocksSc.height);
+                if (blocksSc.row - 2 >= 0 && blocksSc.blockArray[blocksSc.row - 2][column].transform.position.y > blocksSc.height)
+                {
+                    tempPosition.y = blocksSc.blockArray[blocksSc.row - 2][column].transform.position.y + 2;
+                }
+                GameObject block = Instantiate(blocksSc.blocks2d[tempRandomNumber][0], tempPosition, Quaternion.identity);
+                block.name = (blocksSc.row - 1) + "" + column;
+                block.GetComponent<SpriteRenderer>().sortingOrder = blocksSc.row;
+                blocksSc.blockArray[blocksSc.row - 1][column] = block;
+            }
+        }
+    }
+    public void ReBuild()
+    {
+        for (int j = 0; j < blocksSc.blockArrayAsNum[0].Count; j++)
+        {
+            int numOfNewObj = 0;
+
+            for (int i = 0; i < blocksSc.blockArrayAsNum.Count; i++)
+            {
+                if (blocksSc.blockArrayAsNum[i][j] == 10)
+                {
+                    numOfNewObj++;
+                    for (int k = i + 1; k < blocksSc.blockArrayAsNum.Count; k++)
+                    {
+                        if (blocksSc.blockArrayAsNum[k][j] != 10)
+                        {
+                            blocksSc.blockArrayAsNum[i][j] = blocksSc.blockArrayAsNum[k][j];
+                            blocksSc.blockArrayAsNum[k][j] = 10;
+                            numOfNewObj--;
+                            blocksSc.blockArray[i][j] = blocksSc.blockArray[k][j];
+                            blocksSc.blockArray[i][j].GetComponent<SpriteRenderer>().sortingOrder = i;
+                            blocksSc.blockArray[i][j].name = i + "" + j;
+                            break;
+                        }
+                    }                  
+                }             
+            }
+            for (int i = blocksSc.row - numOfNewObj; i < blocksSc.row; i++)
+            {                   
+                int tempRandomNumber = Random.Range(0, blocksSc.numberOfColors);
+                blocksSc.blockArrayAsNum[i][j] = tempRandomNumber;
+                tempPosition = new Vector2(j, 2 + blocksSc.height);
+                if (i - 1 >= 0 && blocksSc.blockArray[i - 1][j].transform.position.y > blocksSc.height)
+                {
+                    tempPosition.y = blocksSc.blockArray[i - 1][j].transform.position.y + 2;
+                }
+                block = Instantiate(blocksSc.blocks2d[tempRandomNumber][0], tempPosition, Quaternion.identity);
+                block.name = i + "" + j;
+                block.GetComponent<SpriteRenderer>().sortingOrder = i;
+                blocksSc.blockArray[i][j] = block;
+            }
+        }
+    }
+}
